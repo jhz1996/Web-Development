c684166fb04022d3e025e7b098eb12b53de64135
jhz996 <jhz1996@ufl.edu>
2015-06-28 00:34:02 -0400

    Web Apps

---
+++ EstimateCubicRoot/EstimateCubicRoot.js
@@ -0,0 +1,168 @@
+﻿
+google.load('visualization', '1', { packages: ['corechart'] });
+google.setOnLoadCallback(function () {
+    angular.bootstrap(document.body, ['estimateRootApp']);
+});
+
+var estimateRootApp = angular.module('estimateRootApp', []);
+estimateRootApp.controller('estimateRootCtrl', function ($scope, $http) {
+//angular.module('estimateRootApp', []).
+//  controller('estimateRootCtrl', ['$scope',function($scope) {
+
+    function derivative(a, b, c, d, xprev) {
+        var deriv = 3 * a * Math.pow(xprev, 2) + 2 * b * Math.pow(xprev, 1) + c * 1;
+
+        return deriv;
+    }
+
+    function f(a, b, c, d, xprev) {
+        var f = a * Math.pow(xprev, 3) + b * Math.pow(xprev, 2) + c * xprev + d * 1;
+
+        return f;
+    }
+
+    $scope.calculateRoot = function () {
+        var x = [];
+        var nmax = 10;
+        var xnext;
+
+        var a = $("#a").val();
+        var b = $("#b").val();
+        var c = $("#c").val();
+        var d = $("#d").val();
+        var prec = $("#prec").val();
+        var i = $("#guess").val();
+
+
+        var j = parseInt(i);
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'Iteration');
+        data.addColumn('number', 'Estimate');
+        var N = new Array();
+        var n = 0;
+        N.push(n);
+        x.push(j);
+
+
+        do {
+
+            var xprev = x[n];
+            var fprev = f(a, b, c, d, xprev);
+            var dprev = derivative(a, b, c, d, xprev);
+            xnext = xprev - (fprev / dprev);
+
+
+            var dx = xnext - xprev;
+            var dx2 = Math.abs(dx);
+
+            x.push(xnext);
+            ++n;
+            N.push(n);
+
+            if (n > nmax) {
+
+                break;
+            }
+
+
+        }
+        while (dx2 > prec);
+
+        if (n > nmax) {
+            $("#error").addClass("selected");
+            $("#error").html("Sorry Maximum Iteration Exceeded");
+            $("#error").addClass("red");
+            setTimeout(function () { $("#error").hide(); }, 3000);
+
+        }
+
+        else if (x.length > 2) {
+            $("#error").html("Success! Approximation completed within iteration limits");
+            $("#error").addClass("blue");
+            setTimeout(function () { $("#error").hide(); }, 3000);
+        }
+
+        var showGraph = $("input:radio[name=choice]:checked").val()
+
+        
+        if (showGraph == "No") {
+
+            $("#visualization").hide();
+
+            $("#head2").hide();
+            $("#error").show();
+            $("#error").html("Best estimate so far is " + xnext);
+        }
+
+        else {
+
+            $("#visualization").show();
+            $scope.showChart = true;
+
+            
+            for (i = 0; i < x.length; i++) {
+                data.addRow([N[i], x[i]]);
+            }
+
+            var nmax = data.getColumnRange(0).max;
+            var xmax = data.getColumnRange(1).max;
+
+            var options = {
+                curveType: "function",
+                width: 600, height: 400,
+                title: "Newton method",
+                hAxis: { title: "Iteration", maxValue: nmax + 1 },
+                vAxis: { title: "Estimate", maxValue: xmax - 1 }
+
+            };
+
+            var chart = {};
+            //$scope.data = data;
+            //$scope.options = options;
+            chart.data = data;
+            chart.options = options;
+            $scope.chart = chart;
+
+            //var container = document.getElementById('visualization');
+            //var chart = new google.visualization.ScatterChart(container);
+            //chart.draw(data, options);
+        } // end of generate graph
+
+    } //end of controller
+
+    function validateMyForm() {
+        return false;
+    }
+
+});
+//angular.module('estimateRootApp', [])
+estimateRootApp.directive('gChart', function () {
+    return {
+        restrict: 'A',
+        link: function ($scope, elm, attrs) {
+            $scope.$watch('chart', function () {
+            //    var type = $scope.chart.type;
+                var chart = " ";
+                //if(type=="1"){
+                    chart = new google.visualization.LineChart(elm[0]);
+                //}
+                //else if(type=="2"){
+                //    chart = new google.visualization.BarChart(elm[0]);
+                //}
+                //else if(type=="3"){
+                //    chart = new google.visualization.ColumnChart(elm[0]);
+                //}
+                //else if(type=="4"){
+                //    chart = new google.visualization.PieChart(elm[0]);
+                //}
+        	
+            //chart.draw($scope.chart.data, $scope.chart.options);
+                    chart.draw($scope.chart.data, $scope.chart.options);
+            },true);
+        } //end of link function
+    } // end of return statement
+}); 
+
+
+
+
---
+++ WeatherApp/weatherServiceApp.js
@@ -0,0 +1,96 @@
+﻿
+google.load('visualization', '1', { packages: ['corechart'] });
+google.setOnLoadCallback(function () {
+    angular.bootstrap(document.body, ['weatherServiceApp']);
+});
+
+var weatherServiceApp = angular.module('weatherServiceApp', []);
+weatherServiceApp.controller('weatherServiceCtrl', function ($scope, $http, $timeout) {
+
+    $scope.getWeatherService = function () {
+
+        var url;
+        var locationBy = $scope.locationBy;
+        var location = $scope.location;
+
+        switch (locationBy) {
+            case "zip":
+                url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + location;
+                break;
+            case "city":
+                url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location;
+                break;
+                
+            case "lanLon":
+                url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + $scope.lat + '&lon=' + $scope.lon;
+                break;
+        }
+
+        $http.get(url).success(function (data) {
+                $scope.weather = data;
+                
+                $scope.showWeather = data.message == null ? true : false;
+                $scope.displayMessage = !$scope.showWeather;   //prints out current weather
+                $("#message").show();
+                $("#message").addClass("red");
+                setTimeout(function () { $("#message").hide(); }, 3000);
+            })
+            .error(function () {
+                $scope.error = "An Error has occured while loading weather data from openweathermap.org!";
+                $scope.loading = false;
+            });
+    } //end of function: getWeatherService()
+
+    $scope.onLocationByChange = function () {
+        //var timer;
+        //if (timer) {
+        //    $timeout.cancel(timer);
+        //}
+        if ($scope.locationBy != 'lanLon') {
+            $scope.showLanlon = false;
+            //timer = $timeout(function () {        //used to enter in default text
+                $scope.location = "---- Enter " + $scope.locationBy + " here ----";
+            //}, 100);
+        } else {
+            $scope.showLanlon = true;
+            //$('#lat').attr('type', 'text');
+            //$('#lon').attr('type', 'text');
+            $scope.lat = "---- Enter lat in degree here ----";
+            $scope.lon = "---- Enter lon in degree here ----";
+        }
+    }
+    
+
+}); // end of controller
+
+//angular.module('weatherServiceApp', [])
+weatherServiceApp.directive('gChart', function () {
+    return {
+        restrict: 'A',
+        link: function ($scope, elm, attrs) {
+            $scope.$watch('chart', function () {
+            //    var type = $scope.chart.type;
+                var chart = " ";
+                //if(type=="1"){
+                    chart = new google.visualization.LineChart(elm[0]);
+                //}
+                //else if(type=="2"){
+                //    chart = new google.visualization.BarChart(elm[0]);
+                //}
+                //else if(type=="3"){
+                //    chart = new google.visualization.ColumnChart(elm[0]);
+                //}
+                //else if(type=="4"){
+                //    chart = new google.visualization.PieChart(elm[0]);
+                //}
+        	
+            //chart.draw($scope.chart.data, $scope.chart.options);
+                    chart.draw($scope.chart.data, $scope.chart.options);
+            },true);
+        } //end of link function
+    } // end of return statement
+}); 
+
+
+
+
